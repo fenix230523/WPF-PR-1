efc8a361c613425cb9da26a09b5b6f86b7a628db
fenix230523 <fenix230523@gmail.com>
2024-12-21 14:40:40 +1000

    Add files via upload

---
+++ WpfApp1/WpfApp1/Window2.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Приложение закрылось");
+            Application.Current.Shutdown();
+            
+
+        }
+        
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog { Filter = "Image Files| *.jpg;*.png;*.gif;*.bpm;" };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BitmapImage bitmapImage =
+                    new BitmapImage(new Uri(openFileDialog.FileName));
+                DisplayImage.Source = bitmapImage;
+            }
+
+            
+        }
+
+        
+
+        
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Box.Text = "Hello";
+            Block.Text = "World";
+        }
+    }
+}
